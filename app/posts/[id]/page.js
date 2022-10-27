4aa4938ac4be7af0d43eb5796df398ac31607b94
GaisericDev <sebastiaancrisan@gmail.com>
2022-10-27 16:03:33 +0200

    load post body and title on click

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,6 +1,18 @@
 import React from "react";
+import { use } from "react";
+
+const getPost = async (id) => {
+  let post = await fetch(`https://dummyjson.com/posts/${id}`);
+  return post.json();
+};
 
 export default function page({ params }) {
   let id = params.id;
-  return <div>{id}</div>;
+  let post = use(getPost(id));
+  return (
+    <div>
+      <p>{post.title}</p>
+      <p>{post.body}</p>
+    </div>
+  );
 }
